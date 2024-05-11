f618c60ee520559f50457c906f8b5aeb6df34820
NAlexH2 <alex@nah-red.net>
2024-05-10 21:14:26 -0700

    Fixing style imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./styles/App.css";
+import "./styles/Index.css";
 
 function App() {
     const [count, setCount] = useState(0);
