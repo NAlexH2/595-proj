a617c67cd125a3747b4d636afe84541e1d630862
Alex Harris <nharris@pdx.edu>
2024-05-14 15:12:06 -0700

    rename file. add container

---
+++ src/routes/species-spacecraft.tsx
@@ -0,0 +1,10 @@
+import Container from "../components/general/container";
+export default function SpeciesSpacecraft() {
+    return (
+        <>
+            <Container>
+                <h1>Species Spacecraft</h1>
+            </Container>
+        </>
+    );
+}
