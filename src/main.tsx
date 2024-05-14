9ceb7f720b766720b420c4ee8fdc37a614c31e35
Alex Harris <nharris@pdx.edu>
2024-05-14 15:11:30 -0700

    added custom router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/App.css";
+import "./styles/index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/home";
+import SpeciesSpacecraft from "./routes/species-spacecraft";
+import SeriesActorStats from "./routes/series-actor-stats";
+import RuntimeStats from "./routes/runtime-stats";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "spacecraft/",
+        element: <SpeciesSpacecraft />,
+    },
+    {
+        path: "performers/",
+        element: <SeriesActorStats />,
+    },
+    {
+        path: "run-date-stats/",
+        element: <RuntimeStats />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
