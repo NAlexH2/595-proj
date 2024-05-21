5b3b021550a75ac304602fcd81c798c47db36c82
Alex Harris <nharris@pdx.edu>
2024-05-21 16:05:24 -0700

    Formatting files based on prettier config

--- src/routes/runtime-stats.tsx
+++ src/routes/runtime-stats.tsx
@@ -1,12 +1,12 @@
 import { Container, NavBar } from "../components/general";
 
 export default function RuntimeStats() {
-    return (
-        <>
-            <Container>
-                <NavBar />
-                <h1>Runtime Stats</h1>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <NavBar />
+        <h1>Runtime Stats</h1>
+      </Container>
+    </>
+  );
 }
