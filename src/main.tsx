5b6b19305cb7d276f14628dfb263cfe3521b9e2c
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2024-05-27 13:33:48 -0700

    fixed file name so page would load

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
+import "./styles/Index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
