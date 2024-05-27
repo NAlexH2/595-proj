50307796a9126060a5107797b1de986e4f075bf7
baylie-r-g <brg5@pdx.edu>
2024-05-26 20:57:34 -0700

    species arrays complete

---
+++ src/components/spacecraft/SpeciesArrays.ts
@@ -0,0 +1,263 @@
+
+const ACraftBuild : string[] = [
+  "Abronian",
+  "Akritirian",
+  "Aksani",
+  "Andorian",
+  "Angosian",
+  "Ankari",
+  "Annari",
+  "Antarian",
+  "Antidean",
+  "Apergosian",
+  "Ardanan",
+  "Areore",
+  "Arkonian",
+  "Arretan",
+  "Axanar"
+]
+
+const BCraftBuild : string[] = [
+  "B'omar",
+  "Ba'Neth",
+  "Ba'ul",
+  "Bajoran",
+  "Bardeezan",
+  "Barolian",
+  "Benkaran",
+  "Benthan",
+  "Benzite",
+  "Bolian",
+  "Borg",
+  "Boslic",
+  "Breen",
+  "Brenari",
+  "Briori",
+  "Brunali" 
+]
+
+const CCraftBuild : string[] = [
+  "Caatati",
+  "Cardassian",
+  "Chessu",
+  "Chokuzan",
+  "Corvallen",
+  "Coverian",
+  "Cravic"
+]
+
+const DCraftBuild : string[] = [
+  "Dekendi",
+  "Deltan",
+  "Denebian",
+  "Denobulan",
+  "Devore",
+  "Dralian",
+  "Dramen",
+  "Drayan",
+  "Drookmani"
+]
+
+const ECraftBuild : string[] = [
+  "Ekosian",
+  "Elasian",
+  "Enolian",
+  "Etanian"
+]
+
+const FCraftBuild : string[] = [
+  "Ferengi",
+  "Flaxian"
+]
+
+const GCraftBuild : string[] = [
+  "Galipotan",
+  "Gelrakian",
+  "Gorn"
+]
+
+const HCraftBuild : string[] = [
+  "Haakonian",
+  "Hazari",
+  "Hekaran",
+  "Hirogen",
+  "Human",
+  "Husnock"
+]
+
+const ICraftBuild : string[] = [
+  "Illyrian",
+  "Imhotep",
+  "Iyaaran"
+]
+
+const JCraftBuild : string[] = [
+  "J'naii",
+  "Jelinian",
+  "Jem'Hadar"
+]
+
+const KCraftBuild : string[] = [
+  "K'normian",
+  "Kaelon",
+  "Kalandan",
+  "Kantare",
+  "Karemma",
+  "Kazon",
+  "Kelvan",
+  "Kinjal",
+  "Klaestron",
+  "Klingon",
+  "Kobali",
+  "Kobheerian",
+  "Kobliad",
+  "Kovaalan",
+  "Kraylor",
+  "Kreetassan",
+  "Krenim",
+  "Kressari",
+  "Kriosian",
+  "Ktarian",
+  "Kyrian",
+  "Kzinti"
+]
+
+const LCraftBuild : string[] = [
+  "Ledosian",
+  "Lissepian",
+  "Lokirrim",
+  "Lysian"
+]
+
+const MCraftBuild : string[] = [
+  "Majalan",
+  "Malcorian",
+  "Malon",
+  "Malosian",
+  "Malurian",
+  "Mawasi",
+  "Mazarite",
+  "Medusan",
+  "Mislenite",
+  "Monean"
+]
+
+const NCraftBuild : string[] = [
+  "Nausicaan",
+  "Nihydron",
+  "Numiri",
+  "Nuu'Bari",
+  "Nygean",
+  "Nyria"
+]
+
+const OCraftBuild : string[] = [
+  "Orion",
+  "Ornaran",
+  "Osaarian"
+]
+
+const PCraftBuild : string[] = [
+  "Pakled",
+  "Pelian",
+  "Phylosian",
+  "Pralor",
+  "Promellian",
+  "Provider"
+]
+
+const QCraftBuild : string[] = [
+  "Qomar",
+  "Quarren"
+]
+
+const RCraftBuild : string[] = [
+  "R'ongovian",
+  "Rakhari",
+  "Rakosan",
+  "Ramuran",
+  "Reptohumanoid",
+  "Retellian",
+  "Rigelian",
+  "Romulan",
+  "Rukani",
+  "Rutian"
+]
+
+const SCraftBuild : string[] = [
+  "Satarran",
+  "Serosian",
+  "Sheliak",
+  "Shepherd",
+  "Sikarian",
+  "Skagaran",
+  "Sphere-Builder",
+  "Srivani",
+  "Subytt",
+  "Suliban"
+]
+
+const TCraftBuild : string[] = [
+  "T'Lani",
+  "Takret",
+  "Talarian",
+  "Talavian",
+  "Talaxian",
+  "Tandaran",
+  "Tarellian",
+  "Taresian",
+  "Tarkalean",
+  "Tellarite",
+  "Telsian",
+  "Terellian",
+  "Terran",
+  "Tesnian",
+  "Thasian",
+  "Tholian",
+  "Triannon",
+  "Trill",
+  "Turei",
+  "Tygarian",
+  "Tzenkethi"
+]
+
+const VCraftBuild : string[] = [
+  "Vaadwaur",
+  "Valakian",
+  "Valerian",
+  "Valtese",
+  "Varro",
+  "Vidiian",
+  "Vissian",
+  "Voth",
+  "Vulcan"
+]
+
+const WCraftBuild : string[] = [
+  "Wadi",
+  "Wisp",
+  "Wysanti"
+]
+
+const XCraftBuild : string[] = [
+  "Xantoras",
+  "Xarantine",
+  "Xepolite",
+  "Xindi-Aquatic",
+  "Xindi-Arboreal",
+  "Xindi-Insectoid",
+  "Xindi-Primate",
+  "Xindi-Reptilian",
+  "Xyrillian"
+]
+
+const YCraftBuild : string[] = [
+  "Yridian"
+]
+
+const ZCraftBuild : string[] = [
+  "Zahl",
+  "Zalkonian",
+  "Zeon",
+  "Zibalian"
+]
