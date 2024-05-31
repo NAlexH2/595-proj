8296dd3d5f3bb9f9c16864f1a7bc8bc4844ab7f1
NAlexH2 <alex@nah-red.net>
2024-05-30 18:40:03 -0700

    header is used elsewhere. renaming to banner

--- src/components/general/banner.tsx
+++ src/components/general/banner.tsx
@@ -1,9 +1,9 @@
-import carina from "../../assets/carina_nebula~large-middle-third.png";
+import carina from "../../assets/carina_nebula-crop.png";
 
 const headerAlt =
   "James Webb Space Telescope NIRCam Image of the \
   'Cosmic Cliffs' in Carina Nebula";
-export default function Header() {
+export default function Banner() {
   return (
     <>
       <img src={carina} alt={headerAlt} className="w-full" />
