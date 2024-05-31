dbffa5b2de80c4128bb302a638b47fce569290b8
NAlexH2 <alex@nah-red.net>
2024-05-30 18:40:35 -0700

    progress on navbar

--- src/components/general/navbar.tsx
+++ src/components/general/navbar.tsx
@@ -1,20 +1,31 @@
-export default function NavBar() {
+export default function NavBar({ pageTitle }: { title: string }) {
+  const home = "/";
+  const spacecraft = "/spacecraft";
+  const performers = "/performers";
+  const runStats = "/run-date-stats";
+
   return (
-    <nav className={"text-red-600"}>
-      <ul>
-        <li>
-          <a href={`/`}>Home</a>
-        </li>
-        <li>
-          <a href={`/spacecraft`}>Species Spacecraft</a>
-        </li>
-        <li>
-          <a href={`/performers`}>Actor Stats</a>
-        </li>
-        <li>
-          <a href={`/run-date-stats`}>Series Run Dates</a>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <div className="flex justify-center">
+        <nav className="mt-4">
+          <a
+            className="me-4 rounded-2xl border-blue-500 p-2 bg-gradient-to-tr from-orange-300 from-5%
+              to-blue-700 text-xl font-extrabold text-transparent bg-clip-text"
+            href={`/`}
+          >
+            Home
+          </a>
+          <a className="me-4" href={`/spacecraft`}>
+            Species Spacecraft
+          </a>
+          <a className="me-4" href={`/performers`}>
+            Actor Stats
+          </a>
+          <a className="me-4" href={`/run-date-stats`}>
+            Series Run Dates
+          </a>
+        </nav>
+      </div>
+    </header>
   );
 }
