5f41d7a18dabcd96f5f6ce7fab4c1af61c0f90e5
NAlexH2 <alex@nah-red.net>
2024-06-01 15:42:02 -0700

    moved the interface to its own file

--- src/components/series-actor-stats/actors.ts
+++ src/components/series-actor-stats/actors.ts
@@ -1,41 +1,11 @@
 import axios from "axios";
-interface ActorStapiResponse {
-  uid: string;
-  name: string;
-  birthName?: string | null;
-  gender?: string | null;
-  dateOfBirth?: string | null;
-  placeOfBirth?: string | null;
-  dateOfDeath?: string | null;
-  placeOfDeath?: string | null;
-  animalPerformer?: boolean;
-  audiobookPerformer?: boolean;
-  cutPerformer?: boolean;
-  disPerformer?: boolean;
-  ds9Performer?: boolean;
-  entPerformer?: boolean;
-  filmPerformer?: boolean;
-  ldPerformer?: boolean;
-  picPerformer?: boolean;
-  proPerformer?: boolean;
-  puppeteer?: boolean;
-  snwPerformer?: boolean;
-  standInPerformer?: boolean;
-  stPerformer?: boolean;
-  stuntPerformer?: boolean;
-  tasPerformer?: boolean;
-  tngPerformer?: boolean;
-  tosPerformer?: boolean;
-  videoGamePerformer?: boolean;
-  voicePerformer?: boolean;
-  voyPerformer?: boolean;
-}
+import ActorStapiResponse from "./actorStapiResponse";
 
 export const actorsArray: ActorStapiResponse[] = [];
 
-export default async function actorsData() {
+export default function actorsData() {
   for (let i = 0; i < 69; i += 1) {
-    await axios
+    axios
       .get(
         "http://stapi.co/api/v2/rest/performer/search?pageSize=100&pageNumber=" +
           i,
@@ -43,8 +13,30 @@ export default async function actorsData() {
       .then(response => {
         filterActors(response.data.performers);
       });
+    console.log(i + 1 + " of 69 complete.");
   }
-  return;
+  return actorsArray;
+}
+
+function filterActors(response: ActorStapiResponse[]) {
+  for (let i = 0; i < response.length; i += 1) {
+    if (isActorFromShow(response[i])) {
+      delete response[i].cutPerformer;
+      delete response[i].puppeteer;
+      delete response[i].audiobookPerformer;
+      delete response[i].animalPerformer;
+      delete response[i].stPerformer;
+      delete response[i].stuntPerformer;
+      delete response[i].videoGamePerformer;
+      delete response[i].voicePerformer;
+      delete response[i].dateOfBirth;
+      delete response[i].dateOfDeath;
+      delete response[i].uid;
+      delete response[i].placeOfBirth;
+      delete response[i].placeOfDeath;
+      actorsArray.push(response[i]);
+    }
+  }
 }
 
 function isActorFromShow(data: ActorStapiResponse): boolean {
@@ -64,29 +56,9 @@ function isActorFromShow(data: ActorStapiResponse): boolean {
   ];
   if (
     showData.some(item => {
-      item === true;
+      return item === true;
     })
   )
     return true;
   return false;
-}
-
-function filterActors(response: ActorStapiResponse[]) {
-  for (let i = 0; i < response.length; i += 1) {
-    if (!isActorFromShow(response[i])) {
-      delete response[i];
-      i -= 1;
-    } else {
-      delete response[i].cutPerformer;
-      delete response[i].puppeteer;
-      delete response[i].audiobookPerformer;
-      delete response[i].animalPerformer;
-      delete response[i].stPerformer;
-      delete response[i].stuntPerformer;
-      delete response[i].videoGamePerformer;
-      delete response[i].voicePerformer;
-      delete response[i].filmPerformer;
-      actorsArray.push(response[i]);
-    }
-  }
 }
