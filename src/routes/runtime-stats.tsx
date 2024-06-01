5b1b0f0693443bf93831cbb290ba443525d8d41c
NAlexH2 <alex@nah-red.net>
2024-05-31 17:55:23 -0700

    added groupfooter

--- src/routes/runtime-stats.tsx
+++ src/routes/runtime-stats.tsx
@@ -1,10 +1,11 @@
-import { NavBar } from "../components/general";
+import { GroupFooter, NavBar } from "../components/general";
 
 export default function RuntimeStats() {
   return (
     <>
       <NavBar title={window.location.href} />
       <h1>Runtime Stats</h1>
+      <GroupFooter />
     </>
   );
 }
