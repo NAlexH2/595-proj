86a1c892daf7015bda42592b8b76f1b76cbb70d3
NAlexH2 <alex@nah-red.net>
2024-06-01 15:43:48 -0700

    actor display mostly complete.

--- src/components/series-actor-stats/index.ts
+++ src/components/series-actor-stats/index.ts
@@ -1,4 +1,6 @@
 import actorsData from "./actors.ts";
 import { actorsArray } from "./actors.ts";
+import ActorStapiResponse from "./actorStapiResponse.ts";
 
 export { actorsData, actorsArray };
+export type { ActorStapiResponse };
--- src/routes/series-actor-stats.tsx
+++ src/routes/series-actor-stats.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { GroupFooter, NavBar } from "../components/general";
+import {
+  ActorStapiResponse,
+  actorsData,
+} from "../components/series-actor-stats";
 
 export default function SeriesActorStats() {
+  const [actorData, setActorData] = useState<ActorStapiResponse[]>();
+  useEffect(() => {
+    const data = actorsData();
+    setActorData(data);
+  }, []);
   return (
     <>
       <NavBar title={window.location.href} />
-
+      {actorData ? (
+        actorData.map(actor => (
+          <div key={actor.name}>
+            {actor.birthName !== null ? actor.birthName : actor.name}
+          </div>
+        ))
+      ) : (
+        <div className="mt-10">hi</div>
+      )}
       <GroupFooter />
     </>
   );
