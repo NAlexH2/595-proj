31c00ace1c5d449a6b08c4d03ffc7bd1220b3332
NAlexH2 <alex@nah-red.net>
2024-06-02 16:40:22 -0700

    actors numbers and names properly gathered. wip

--- src/routes/series-actor-stats.tsx
+++ src/routes/series-actor-stats.tsx
@@ -1,36 +1,39 @@
 import { useEffect, useRef, useState } from "react";
 import { GroupFooter, NavBar } from "../components/general";
-import {
-  ActorStapiResponse,
-  actorsData,
-} from "../components/series-actor-stats";
+import { actData } from "../components/series-actor-stats";
 
 export default function SeriesActorStats() {
   const hasFetched = useRef(false);
-  const [actorData, setActorData] = useState<ActorStapiResponse[]>();
+  const [loading, setLoading] = useState(true);
+  const [actorData, setActorData] = useState<{ [key: string]: string[] }>({});
+
   useEffect(() => {
     if (!hasFetched.current) {
       hasFetched.current = true;
-      const data = actorsData();
-      setActorData(data);
+      (async () => {
+        const data = await actData();
+        setActorData(data);
+        setLoading(false);
+      })();
     }
   }, []);
+
   return (
     <>
       <NavBar title={window.location.href} />
-      {actorData ? (
-        actorData.map(actor => (
-          <div key={actor.uid}>
-            {actor.birthName !== null
-              ? "Actor Name: " +
-                actor.name +
-                " ---- Actor Birth Name: " +
-                actor.birthName
-              : "Actor Name: " + actor.name}
+
+      {loading ? (
+        <section className="text-center mt-10 mb-20 text-2xl animate-pulse">
+          There is <div className="font-extrabold inline-block">a lot</div> of
+          data to collect, please wait...
+        </section>
+      ) : (
+        Object.entries(actorData).map(([show]) => (
+          <div key={show}>
+            <h2>{show}</h2>
+            {show.length}
           </div>
         ))
-      ) : (
-        <div className="mt-10">hi</div>
       )}
       <GroupFooter />
     </>
