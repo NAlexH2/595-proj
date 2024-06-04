8880837eb5955237665ae8490715ece3dc1f02b4
NAlexH2 <alex@nah-red.net>
2024-06-03 17:02:17 -0700

    added test id to support testing

--- src/routes/runtime-stats.tsx
+++ src/routes/runtime-stats.tsx
@@ -2,10 +2,10 @@ import { GroupFooter, NavBar } from "../components/general";
 
 export const RuntimeStats = () => {
   return (
-    <>
+    <div data-testid="runtime-test">
       <NavBar title={window.location.href} />
       <h1>Runtime Stats</h1>
       <GroupFooter />
-    </>
+    </div>
   );
 };
--- src/routes/species-spacecraft.tsx
+++ src/routes/species-spacecraft.tsx
@@ -2,10 +2,10 @@ import { GroupFooter, NavBar } from "../components/general";
 
 export const SpeciesSpacecraft = () => {
   return (
-    <>
+    <div data-testid="spacecraft-test">
       <NavBar title={window.location.href} />
       <h1>Species Spacecraft</h1>
       <GroupFooter />
-    </>
+    </div>
   );
 };
